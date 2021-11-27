ed36b7caf787558f5128455c79cda07c46829e9e
outplayedru <malofeevs00@gmail.com>
2021-11-27 17:11:44 +0300

    zadaniya 3,4

---
+++ Задание_3/3.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System;
+class Program
+{
+    static int plus = 0;
+    private static object stop = new object();
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            Thread myThread = new Thread(Inc);
+            myThread.Start();
+        }
+    }
+  
+    public static void Inc()
+    {
+        lock (stop)
+        {
+            plus++;
+            Console.WriteLine(plus);
+        }
+    }
+}
---
+++ Задание_3/4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static Semaphore _pool;
+        private static int _padding;
+
+        public static void Main()
+        {
+            _pool = new Semaphore(0, 3);
+            for (int i = 0; i < 3; i++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(MyThreads));
+                t.Start(i);
+                t.Name = "thread : " + i.ToString();
+            }   
+            _pool.Release(3);   
+        }
+        
+        private static void MyThreads(object threadN)
+        { 
+            Console.WriteLine($"Start {Thread.CurrentThread.Name}");
+            _pool.WaitOne();
+            int padding = Interlocked.Add(ref _padding, 1000);
+            Thread.Sleep(1000 + padding); 
+            Console.WriteLine($"Thread {threadN} = " + _pool.Release());
+        }
+    }
+}
